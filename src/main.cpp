2a8e81c3b6e0880bb46501591789fb91c9f4af84
Tamino Bauknecht <dev@tb6.eu>
2022-04-03 20:16:33 +0200

    refactor code to run tflite and move to tflite_runner class

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,14 @@
 #include "tensorflow/lite/model_builder.h"
 #include "tensorflow/lite/kernels/register.h"
 #include "arguments.h"
+#include "tflite_runner.h"
 #include <iostream>
 #include <memory>
 
 int main(int argc, char* argv[])
 {
 	auto args = arguments(argc, argv);
-	auto model = tflite::FlatBufferModel::BuildFromFile(args.model_path.c_str());
-	if(model == nullptr)
-	{
-		std::cerr << "Failed to load model (" << args.model_path << ")\n";
-		return 1;
-	}
-
-	std::unique_ptr<tflite::Interpreter> interpreter;
-	tflite::ops::builtin::BuiltinOpResolver resolver;
-	if(tflite::InterpreterBuilder(*model, resolver)(&interpreter) != kTfLiteOk)
-		return 2;
-	if(interpreter->AllocateTensors() != kTfLiteOk)
-		return 3;
-
-	for(auto i : interpreter->inputs())
-		*interpreter->typed_tensor<float>(i) = args.input;
-
-	interpreter->Invoke();
-
-	for(auto i : interpreter->outputs())
-		std::cout << *interpreter->typed_tensor<float>(i) << std::endl;
+	auto runner = tflite_runner(args.model_path);
+	for(auto output : runner.run({args.input}))
+		std::cout << "Input: " << args.input << "\nOutput: " << output << std::endl;
 }
---
+++ src/tflite_runner.h
@@ -0,0 +1,42 @@
+#ifndef TFLITE_RUNNER_H
+#define TFLITE_RUNNER_H
+
+#include "tensorflow/lite/interpreter_builder.h"
+#include "tensorflow/lite/model_builder.h"
+#include "tensorflow/lite/kernels/register.h"
+
+class tflite_runner
+{
+	std::unique_ptr<tflite::FlatBufferModel> model;
+	std::unique_ptr<tflite::Interpreter> interpreter;
+
+public:
+	tflite_runner(std::string_view model_path) :
+	 model{tflite::FlatBufferModel::BuildFromFile(model_path.data())}
+	{
+		if(model == nullptr)
+			throw std::runtime_error{std::string{"Failed to load model ("} + model_path.data() + ")\n"};
+		tflite::ops::builtin::BuiltinOpResolver resolver;
+		if(tflite::InterpreterBuilder(*model, resolver)(&interpreter) != kTfLiteOk)
+			throw std::runtime_error{"failed to build interpreter"};
+		if(interpreter->AllocateTensors() != kTfLiteOk)
+			throw std::runtime_error{"unable to allocate tensors"};
+	}
+
+	std::vector<float> run(const std::vector<float>& inputs)
+	{
+		if(inputs.size() != interpreter->inputs().size())
+			throw std::invalid_argument{"input size does not match input of model"};
+		for(int i = 0; i < inputs.size(); ++i)
+			*interpreter->typed_tensor<float>(interpreter->inputs()[i]) = inputs[i];
+
+		interpreter->Invoke();
+
+		std::vector<float> result;
+		for(auto i : interpreter->outputs())
+			result.push_back(*interpreter->typed_tensor<float>(i));
+		return result;
+	}
+};
+
+#endif // TFLITE_RUNNER_H
